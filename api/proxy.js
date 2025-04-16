c9accc6048622d37e96e33c5580d3e6df41734a9
diablo0olbaid <104808307+diablo0olbaid@users.noreply.github.com>
2025-04-16 11:18:36 -0300

    proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,23 +1,12 @@
 export default async function handler(req, res) {
   const input = req.query.input;
-
-  if (!input) {
-    return res.status(400).json({ error: "Falta input" });
-  }
+  const endpoint = `https://recetario-ffv9c6zr4-gastons-projects-b2e4ce12.vercel.app/api/receta?input=${encodeURIComponent(input)}`;
 
   try {
-    const response = await fetch(
-      "https://recetario-4j79civwf-gastons-projects-b2e4ce12.vercel.app/api/receta?input=" + encodeURIComponent(input)
-    );
-
-    if (!response.ok) {
-      return res.status(500).json({ error: "Respuesta no válida", status: response.status });
-    }
-
+    const response = await fetch(endpoint);
     const data = await response.json();
     return res.status(200).json(data);
   } catch (err) {
-    console.error("Error real en proxy:", err);
-    return res.status(500).json({ error: "Fallo al hacer fetch desde el proxy", detalle: err.message });
+    return res.status(500).json({ error: 'Fallo al generar receta desde el proxy' });
   }
 }
